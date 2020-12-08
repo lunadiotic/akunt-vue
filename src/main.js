7a86bf5f74c453f600ada94d34b9fd2c97deab5d
IDStack <idstack@outlook.co.id>
2020-12-09 00:22:59 +0700

    install dependency and create transaction component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+
+// import bootstrap, popper
+import 'bootstrap/dist/css/bootstrap.css'
+import 'popper.js/dist/popper'
+import 'bootstrap/dist/js/bootstrap'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
